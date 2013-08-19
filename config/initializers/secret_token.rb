a09763166d2f2fda1f8c74893b5792ab47193d16
joueur <yves.tatienne@gmail.com>
2013-08-19 18:57:43 +0000

    INIT

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Voiture::Application.config.secret_key_base = '4c45ef9682cf7d14d4870af2dc7949071dc41f704250cef5e3f39f0237039f96c0f4cdc41179616c7cc3c98d0613e23e16f58b4e85de00f53186a228c2ead78a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Voiture::Application.config.session_store :cookie_store, key: '_voiture_session'
